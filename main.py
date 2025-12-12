6236166460f3668f7556b2b706d5c9670e979d1a
matisimone-halo <matias.simone@halopowered.com>
2025-12-12 14:11:37 -0300

    added tavily search

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from typing import List
+
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
+from langchain_tavily import TavilySearch
+
 # from tavily import TavilyClient
 
 # tavily = TavilyClient()
@@ -25,15 +30,34 @@ from langchain_groq import ChatGroq
 # tools = [search]
 # agent = create_agent(model=llm, tools=tools)
 
-from langchain_tavily import TavilySearch
+class Source(BaseModel):
+    """Schema for a source user by the agent"""
+    # name: str = Field(description="Name of the source")
+    url: str = Field(description="URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response"""
+    answer: str = Field(description="Answer to the user")
+    sources: List[Source] = Field(default_factory=list, description="Sources used to answer the user")
+
+
 llm = ChatGroq(temperature=0, model="openai/gpt-oss-120b")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content='find 2 jobs for angular front end developers in linkedin adn give me just the links')})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="find 2 jobs for angular front end developers in linkedin adn give me just the links"
+            )
+        }
+    )
     print(result)
+    # print(result['structured_response'].answer)
+    # print(result['structured_response'].sources)
 
 
 if __name__ == "__main__":
